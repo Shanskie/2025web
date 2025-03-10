a4437bb01fa660869018cf31ca70dbf70b77ce25
Shanskie <muhammadikhsanramadhan88@gmail.com>
2025-03-10 13:06:48 +0800

    create prodi.php

--- index.php
+++ index.php
@@ -7,5 +7,34 @@
 </head>
 <body>
     <h1>DATA MAHASISWA</h1>
+    <br>
+    <br>
+    <table border="1" cellspacing="0" cellpadding="5">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>NIM</th>
+                <th>Nama</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>19001001</td>
+                <td>Andi Santoso</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>19001002</td>
+                <td>Budi Wibowo</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>19001003</td>
+                <td>Candra Widodo</td>
+            </tr>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
